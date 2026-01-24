b6475ef38126ed8e4987d6f5371846abb37801c5
lucasmacori <25822355+lucasmacori@users.noreply.github.com>
2026-01-24 21:29:22 +0100

    feat: add discord theme and fix install script

---
+++ misc/README.md
@@ -0,0 +1,7 @@
+# Misc files
+
+## Discord
+
+- Install BetterDiscord
+- Install system24 theme from the BetterDiscord marketplace
+- Use custom css in the BetterDiscord config
--- setup-arch.sh
+++ setup-arch.sh
@@ -7,13 +7,13 @@ sudo pacman -S yay
 # Window manager (Hyprland)
 echo "Installing Hyprland..."
 yay no-confirm -S hypr clipse
-sudo pacman -Sy greetd greetd-tuigreet xdg-desktop-portal-hyprland
+sudo pacman -Sy greetd greetd-tuigreet xdg-desktop-portal-hyprland hyprlock
 sudo cp ./install_resources/greetd.config.toml /etc/greetd/config.toml
 sudo systemctl enable --now greetd
 
 # Terminal, CLIs and TUIs
 echo "Installing terminal tools..."
-sudo pacman -Sy alacritty vim nvim tmux git swww lazygit brightnessctl jq less openssh fish zoxide ttf-firacode-nerd rofi mako
+sudo pacman -Sy alacritty vim nvim tmux git swww lazygit brightnessctl jq less openssh fish zoxide ttf-firacode-nerd rofi mako quickshell
 chsh -s $(which fish)                        # Using fish as default shell
 curl -sS https://starship.rs/install.sh | sh # Starship prompt
 
@@ -35,10 +35,12 @@ cp ./.gitconfig ~/.gitconfig
 
 # Rumda base theme installation
 echo "Installing base Rumda theme"
+sudo chmod +x ./.config/rumda/install.sh
+./.config/rumda/install.sh
 
 # Setup dotfiles
 echo "Setting dotfiles and overwriting theme with custom config..."
 mkdir -p ~/.config
-cp -r ./.config/* ~/.config
+cp -r ./.config/* ~/.config/
 
 echo "All done! You can start Hyprland using the 'start-hyprland' command"
