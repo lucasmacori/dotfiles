285ba47ca18236a6a06d1a11aaa37398fc6a91f2
Lucas MACORI <25822355+lucasmacori@users.noreply.github.com>
2026-01-25 19:33:57 +0100

    feat: add MacOS dotfiles

--- .config/nvim/lua/plugins/autocomplete.lua
+++ .config/nvim/lua/plugins/autocomplete.lua
@@ -1,24 +1,26 @@
 return {
   {
-    "hrsh7th/cmp-nvim-lsp"
-  },
-  {
     "L3MON4D3/LuaSnip",
     dependencies = {
-      "saadparwaiz1/cmp_luasnip",
-      "rafamadriz/friendly-snippets"
-    }
+      "rafamadriz/friendly-snippets",
+    },
   },
   {
     "hrsh7th/nvim-cmp",
+    event = "InsertEnter", -- Load when entering insert mode
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp", -- Move this here
+      "saadparwaiz1/cmp_luasnip", -- This too
+      "hrsh7th/cmp-buffer", -- Add buffer completions
+      "hrsh7th/cmp-path", -- Add path completions
+    },
     config = function()
-      local cmp = require 'cmp'
+      local cmp = require("cmp")
       require("luasnip.loaders.from_vscode").lazy_load()
-
       cmp.setup({
         snippet = {
           expand = function(args)
-            require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+            require("luasnip").lsp_expand(args.body)
           end,
         },
         window = {
@@ -26,30 +28,30 @@ return {
           documentation = cmp.config.window.bordered(),
         },
         mapping = cmp.mapping.preset.insert({
-          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-          ['<C-f>'] = cmp.mapping.scroll_docs(4),
-          ['<C-Space>'] = cmp.mapping.complete(),
-          ['<C-e>'] = cmp.mapping.abort(),
-          ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<C-e>"] = cmp.mapping.abort(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
         }),
         sources = cmp.config.sources({
-          -- { name = 'nvim_lsp' },
-          { name = 'luasnip' }, -- For luasnip users.
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
         }, {
-          { name = 'buffer' },
-        })
+          { name = "buffer" },
+          { name = "path" }, -- Add path completions
+        }),
       })
-
       vim.api.nvim_create_autocmd("InsertLeave", {
         callback = function()
           if
-              require("luasnip").session.current_nodes[vim.api.nvim_get_current_buf()]
-              and not require("luasnip").session.jump_active
+            require("luasnip").session.current_nodes[vim.api.nvim_get_current_buf()]
+            and not require("luasnip").session.jump_active
           then
             require("luasnip").unlink_current()
           end
         end,
       })
-    end
-  }
+    end,
+  },
 }
--- README.md
+++ README.md
@@ -17,10 +17,16 @@ cd dotfiles
 sudo chmod +x setup-arch.sh
 ```
 
-## Theme
+### Theme
 
 Hyprland and nvim themes are based on rumda config : https://github.com/Nytril-ark/rumda/tree/main
 
 They have been edited to fit my taste but most of it is used.
 
 Keybindings are different, and layout is FR instead of US as I'm using an AZERTY keyboard.
+
+## Setup a Mac
+
+aerospace config is found in .aeropsace.toml.
+
+For alacritty and nvim, the files in .config are fine.
