b7af718522ee075600f7360662620a03374e8654
lucasmacori <25822355+lucasmacori@users.noreply.github.com>
2026-01-25 19:36:50 +0100

    feat: updated fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,7 @@
 if status is-interactive
-# Commands to run in interactive sessions can go here
+    # Commands to run in interactive sessions can go here
 end
 
 eval "$(zoxide init fish)"
 starship init fish | source
+export PATH="$HOME/.local/bin:$PATH"
